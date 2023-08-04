9cd7d3464baf57b51a661300639c88b8e24b5b02
Vladyslav Lopatin <vladyslav.lopatin@wskz.pl>
2023-08-04 10:44:09 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# Publishing
+### Views
+```php
+php artisan vendor:publish --tag="laravel-subscription-views"
+```
+
+### Config
+```php
+php artisan vendor:publish --tag="laravel-subscription-config"
+```
---
+++ src/Models/ActivityLog.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Atin\LaravelActivitylog\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+
+class ActivityLog extends Model
+{
+    protected $table = 'activity_log';
+}
